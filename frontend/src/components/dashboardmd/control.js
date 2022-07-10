5ffc4f6c464a90c670c7233d41acc2f173b02755
Gabriela López <denisgabylopez@gmail.com>
2022-07-10 14:28:58 -0500

    elimina seguimiento variable velocidad en md dashboard

--- frontend/src/components/dashboardmd/control.js
+++ frontend/src/components/dashboardmd/control.js
@@ -35,7 +35,6 @@ export const Control = () => {
         data.map((item) => {
             setAngulo(item.angulo);
             setTiempo(item.tiempoActual);
-            setVelocidad(item.velocidad);
         });
       }
 

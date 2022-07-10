355d9fb8b85c9a80b8f51e792fb2c1b6a85a7c9d
Gabriela López <denisgabylopez@gmail.com>
2022-07-10 14:30:07 -0500

    try/except manejo conexión servidor

--- backend/src/api-flask/clienteOpcua.py
+++ backend/src/api-flask/clienteOpcua.py
@@ -14,7 +14,7 @@ async def set_let():
         res = "Error servidor"
     return res
 
-async def set_params(flex, ext, vel, time, sost, rep):
+async def set_params(flex, ext, vel, time, sost, play):
     res = ''
     url = "opc.tcp://localhost:4840/freeopcua/server/"
     try:
@@ -22,9 +22,11 @@ async def set_params(flex, ext, vel, time, sost, rep):
             uri = "telerehab:opcua"
             idx = await client.get_namespace_index(uri)
             test = await client.nodes.root.get_child(["0:Objects", "2:Parametros"])
-            res = await test.call_method("2:SendParams", flex, ext, vel, time, sost, rep)
-    except:
-        res = "Error servidor"
+            resServ = await test.call_method("2:SendParams", flex, ext, vel, time, sost, play)
+            if resServ:
+                res = {'mensaje': 'OK'}
+    except Exception as e:
+        res = {'mensaje': 'Error servidor', 'error': e}
     return res
 
 async def send_id(idSesion):
@@ -35,6 +37,9 @@ async def send_id(idSesion):
             uri = "telerehab:opcua"
             idx = await client.get_namespace_index(uri)
             test = await client.nodes.root.get_child(["0:Objects", "2:Parametros"])
-            res = await test.call_method("2:GetIdSesion", idSesion)
-    except:
-        res="Error servidor"
+            resServ = await test.call_method("2:GetIdSesion", idSesion)
+        if resServ:
+                res = {'mensaje': 'OK'}
+    except Exception as e:
+        res = {'mensaje': 'Error servidor', 'error': e}
+    return res
